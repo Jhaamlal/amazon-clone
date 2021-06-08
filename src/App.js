2982429703c0e4a88ce0c44c97af30120612d071
ravi shankar <shankarji1729@gmail.com>
2021-06-08 10:28:50 +0530

    Header desined

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Headers from "./Headers";
 
 function App() {
   return (
-    <div className="App">
-      Ram
-    </div>
+    <Router>
+      <div className="App">
+        <switch>
+          <Route path="/checkout">
+            <h1>Checkout</h1>
+          </Route>
+          <Route path="/login">
+            <h2>Login</h2>
+          </Route>
+          <Route path="/" exact>
+            {/* <h1>Home page</h1> */}
+            <Headers />
+          </Route>
+        </switch>
+      </div>
+    </Router>
   );
 }
 
